db408df282a55812a70feae61d81d814446b559c
Misgana Daniel <98047159+MisgunDaniel@users.noreply.github.com>
2022-01-24 21:31:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+var rent = document.getElementById("rent").value;
+var rentSelect = document.getElementById("homeSelect").value;
+
+//main calculate option
+var cal = document.getElementById("Calculate").value;
+
+function homeFunction(){
+    if(cal == 1){
+        if(rentSelect == 1){
+            rent = rent;
+        }else if(rentSelect == 2){
+            rent = rent/4;
+        }
+    }
+    document.getElementById("homeResult").innerHTML = rent+" ETB";
+}
+
+function calculate(){
+
+}
